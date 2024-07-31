f5bedf050be108fb5169f8737716505182100946
Angelo Sevhen <vhanges7777@gmail.com>
2024-07-31 18:15:09 +0800

    Finished configuring database and database connection configuration

---
+++ config/talablogDB.php
@@ -0,0 +1,18 @@
+<?php
+
+ class  Db{
+    private $host =  "localhost";
+    private $username =  "root";
+    private $password =  "";
+    private $dbName=  "talablogDB";
+
+    protected function connect(){
+        $credentials = "mysql:host=$this->host;dbname$this->dbName";
+
+        $pdo = new PDO($credentials, $this->username, $this->password);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $pdo;
+
+    }
+
+}
